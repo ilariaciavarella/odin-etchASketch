114bfd817057e5f49934e16efb8ae010e6fb1a91
Ilaria <128404113+ilariaciavarella@users.noreply.github.com>
2023-04-22 22:47:52 +0200

    Make first three buttons work

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const pad = document.querySelector("#pad");
 
-let gridSize = 16;
+const rainbowBtn = document.querySelector('#rainbowBtn');
+const blackBtn = document.querySelector('#blackBtn');
+const eraserBtn = document.querySelector('#eraserBtn');
+const sizeBtn = document.querySelector('#sizeBtn');
+const clearBtn = document.querySelector('#clearBtn');
 
 // Create grid
-for (let i = 0; i < gridSize; i++) {
-  const singleRow = document.createElement("div");
-  singleRow.setAttribute("class", "singleRow");
-  for (let j = 0; j < gridSize; j++) {
-    const squares = document.createElement("div");
-    squares.setAttribute("class", "singleSq");
-    singleRow.appendChild(squares);
-  }
-  pad.appendChild(singleRow);
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+      const singleRow = document.createElement("div");
+      singleRow.setAttribute("class", "singleRow");
+      for (let j = 0; j < gridSize; j++) {
+        const squares = document.createElement("div");
+        squares.setAttribute("class", "singleSq");
+        singleRow.appendChild(squares);
+      }
+      pad.appendChild(singleRow);
+    }
 }
+
+createGrid(16);
 
 //Generate random color
 function getRandomColor() {
@@ -24,13 +32,44 @@ function getRandomColor() {
 
 // Set hover effect
 const singleSq = document.querySelectorAll(".singleSq");
-//singleSq.forEach(square => square.addEventListener('mouseover', () => {square.setAttribute('style', 'background-color: black;')}))
-singleSq.forEach((square) =>
-  square.addEventListener(
-    "mouseover",
-    () => {
-      square.setAttribute("style", `background-color: rgb(${getRandomColor()});`);
-    },
-    { once: true }
-  )
-);
+
+// Make buttons clickable
+function useBlack() {
+    singleSq.forEach(square => square.addEventListener('mouseover', () => {square.setAttribute('style', 'background-color: black;')}))
+}
+
+blackBtn.addEventListener('click', useBlack);
+blackBtn.addEventListener('click', () => {
+    blackBtn.classList.add('clicked');
+    rainbowBtn.classList.remove('clicked');
+    eraserBtn.classList.remove('clicked');
+});
+
+function useRainbow() {
+    singleSq.forEach((square) =>
+    square.addEventListener(
+        "mouseover",
+        () => {
+            square.setAttribute("style", `background-color: rgb(${getRandomColor()});`);
+        }
+        )
+        );
+    }
+    
+rainbowBtn.addEventListener('click', useRainbow);
+rainbowBtn.addEventListener('click', () => {
+    blackBtn.classList.remove('clicked');
+    rainbowBtn.classList.add('clicked');
+    eraserBtn.classList.remove('clicked');
+});
+
+function useEraser() {
+    singleSq.forEach(square => square.addEventListener('mouseover', () => { square.setAttribute('style', 'background-color: rgb(245, 245, 245);') }))
+}
+
+eraserBtn.addEventListener('click', useEraser);
+eraserBtn.addEventListener('click', () => {
+    eraserBtn.classList.add('clicked');
+    rainbowBtn.classList.remove('clicked');
+    blackBtn.classList.remove('clicked');
+});
