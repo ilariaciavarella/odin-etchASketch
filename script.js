a09cf4f4c889462f0ac439c97e0d9179db65ea75
Ilaria <128404113+ilariaciavarella@users.noreply.github.com>
2023-04-22 16:06:23 +0200

    Create grid

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const pad = document.querySelector('#pad');
 
-let gridDim = 16;
-let innerBlock;
+let gridSize = 16;
 
-for (let i = 0; i < gridDim; i++) {
-    innerBlock = document.createElement('div');
-    innerBlock.setAttribute('class', 'inner');
-    pad.appendChild(innerBlock);
+for(let i = 0; i < gridSize; i++) {
+    const singleRow = document.createElement('div');
+    singleRow.setAttribute('class', 'singleRow');
+    for (let j = 0; j < gridSize; j++) {
+        const squares = document.createElement('div');
+        squares.setAttribute('class', 'singleSq');
+        singleRow.appendChild(squares);
+    }
+    pad.appendChild(singleRow);
 }
-
